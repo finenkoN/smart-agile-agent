201ed373ec16a40539ca2f3b9f7ec8a35b5fe97f
Nikita <nikitapickf@gmail.com>
2026-03-28 01:57:06 +0300

    stage 1

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
 
 func main() {
-	fmt.Println("Privet!")
+
+	godotenv.Load()
+
+	apiKey := os.Getenv("GROQ_API_KEY")
+	baseURL := os.Getenv("GROQ_BASE_URL")
+
+	config := openai.DefaultConfig(apiKey)
+	config.BaseURL = baseURL
+
+	client := openai.NewClientWithConfig(config)
+
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: "openai/gpt-oss-120b",
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello",
+				},
+			},
+		},
+	)
+	fmt.Println(resp.Choices[0].Message.Content)
 }
